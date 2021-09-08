c53b48ddda980dc417d7ecd4d551e30fc35ef962
surajidk <surajidk12@gmail.com>
2021-09-08 09:19:14 +0700

    component and props

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,49 @@
-`use strict`
+`use strict`;
 
-function tick() {
-    const element = (
-        <div>
-            <h1>Hello, React</h1>
-            <h2>It is, {new Date().toLocaleTimeString()}</h2>
-        </div>
-    )
-
-    ReactDOM.render(
-        element,
-        document.getElementById('root')
-    );
+function formatDate(e) {
+  return e.toLocaleDateString();
 }
 
-setInterval(tick,1000)
+function Avatar(props) {
+  return (
+    <img className="Avatar" src={props.user.avatarUrl} alt={props.user.name} />
+  );
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">{props.user.name}</div>
+    </div>
+  );
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">{props.text}</div>
+      <div className="Comment-date">{formatDate(props.date)}</div>
+    </div>
+  );
+}
+
+const comment = {
+  text: `Hello Kitty`,
+  comment: `I hope you enjoy learning react!`,
+  date: new Date(),
+  author: {
+    name: `bird`,
+    avatarUrl: `https://placekitten.com/g/64/64`,
+  },
+};
+
+ReactDOM.render(
+  <Comment
+    date={comment.date}
+    text={comment.text}
+    author={comment.author}
+  />,
+  document.getElementById("root")
+);
