465f8dda53ef38d0d7cde340986b7344e88be6fc
surajidk <surajidk12@gmail.com>
2021-09-09 19:51:59 +0700

    list key

--- assets/js/custom/clock.js
+++ assets/js/custom/clock.js
@@ -1,106 +1,60 @@
 `use strict`;
-class LoginControl extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isLoginIn: false, showWarning: false };
-    this.handleLoginClick = this.handleLoginClick.bind(this);
-    this.handleLogoutClick = this.handleLogoutClick.bind(this);
-  }
 
-  handleLoginClick() {
-    this.setState({ isLoginIn: true });
-  }
-
-  handleLogoutClick() {
-    this.setState({ isLoginIn: false });
-  }
-
-  render() {
-    const isLoginIn = this.state.isLoginIn;
-    let button;
-
-    // if (isLoginIn) {
-    //   button = <button onClick={this.handleLogoutClick}>Logout</button>;
-    // } else {
-    //   button = <button onClick={this.handleLoginClick}>Login</button>;
-    // }
-    {
-      isLoginIn
-        ? (button = <button onClick={this.handleLogoutClick}>Logout</button>)
-        : (button = (
-            <div>
-              <WarningBanner warn={this.state.showWarning} />
-              <button onClick={this.handleLoginClick}>Login</button>
-            </div>
-          ));
-    }
-
-    return (
-      <div>
-        <Greeting isLoginIn={isLoginIn} />
-
-        {button}
-      </div>
-    );
-  }
+function ListItem(props) {
+  return <li>{props.value}</li>;
 }
 
-function WarningBanner(props) {
-  if (props.warn) {
-    return null;
-  }
-
-  return (
-    <div className="warning">
-      Warning!
-      <GuestGreeting />
-    </div>
-  );
-}
-
-function UserGreeting(props) {
-  return <h1>Welcome brother</h1>;
-}
-
-function GuestGreeting(props) {
-  return <h1>Please sign in</h1>;
-}
-
-function Greeting(props) {
-  const isLoginIn = props.isLoginIn;
-  if (isLoginIn) {
-    return (
-      <div>
-        <UserGreeting />
-        <MailBox unreadMessages={message} />
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <GuestGreeting />
-      </div>
-    );
-  }
-}
-
-function MailBox(props) {
-  const unreadMessages = props.unreadMessages;
+function NumberList(props) {
+  const numbers = props.numbers;
+  let listItems = numbers.map((number, index) => (
+    <ListItem key={index} value={number} />
+  ));
   return (
     <div>
-      {unreadMessages.length > 0 && (
-        <h2>You Have {unreadMessages.length} unread Messages</h2>
-      )}
+      <ul>{listItems}</ul>
     </div>
   );
 }
 
-const message = ["react", "re:react", "re:re:react"];
+function Post(props) {
+  return (
+    <div >
+      <h3>{props.content.title}</h3>
+      <p>{props.content.content}</p>
+    </div>
+  );
+}
+
+function Blog(props) {
+  const sidebar = (
+    <ul>
+      {props.posts.map((post, index) => (
+        <li key={index}>{post.title}</li>
+      ))}
+    </ul>
+  );
+  const content = props.posts.map((post, index) => (
+    <Post key={index} content={post} />
+  ));
+  return (
+    <div>
+      {sidebar} <hr /> {content}
+    </div>
+  );
+}
+
+const numbers = [4, 5, 3, 2, 6, 3, 2, 4, 1, 2, 5, 3, 2];
+// const numbers = [1, 2, 3, 4, 5];
+const posts = [
+  { id: 1, title: "Hello World", content: "Welcome to learning React!" },
+  { id: 2, title: "Installation", content: "You can install React from npm." },
+];
 
 function App() {
   return (
     <div>
-      <LoginControl />
+      <NumberList numbers={numbers} />
+      <Blog posts={posts} />
     </div>
   );
 }
